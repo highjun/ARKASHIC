ef7e0c19245634781578c567319231a837293f44
highjun <83162738+highjun@users.noreply.github.com>
2026-09-13 22:37:08 +0000

    storybook: push 988db444904c67c99714aee37df2ec9aa5d6ffac

---
+++ assets/TextEditor.stories-CrtWhtQ0.js
@@ -0,0 +1,10 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-CXqGf8kE.js";import{n,t as r}from"./TextEditor-DWxNNfYH.js";var i,a,o,s,c,l,u,d,f;function p(){return(p=e((()=>{r(),i=t(),a={title:`filesystem/TextEditor`,component:n,decorators:[e=>(0,i.jsx)(`div`,{style:{height:480,width:720},children:(0,i.jsx)(e,{})})],args:{path:`src/main.ts`,content:`import { greet } from './greet';
+
+// 진입점
+export const main = (): void => {
+  const message = greet('ARKASHIC');
+  console.log(message);
+};
+
+main();
+`}},o={},s={args:{content:``}},c={args:{loading:!0}},l={args:{chrome:`none`}},u={args:{readOnly:!1,onChange:()=>void 0,onSave:()=>void 0,isDirty:!0}},d={args:{readOnly:!1,onChange:()=>void 0,onSave:()=>void 0,isDirty:!0,isSaving:!0}},f=[`Default`,`Empty`,`Loading`,`NoChrome`,`Editable`,`Saving`]})))()}p();export{o as Default,u as Editable,s as Empty,c as Loading,l as NoChrome,d as Saving,f as __namedExportsOrder,a as default};
---
+++ assets/iframe-BhBIthWL.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-D7vlOShI.js";e();
